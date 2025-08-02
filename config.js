c01a4d9b42f563fba8e61b84eeac532bdc4104c5
PopinaBudapest <popinabp@gmail.com>
2025-08-02 20:30:55 +0200

    Inital productive version

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  sheetId: "1D4m0Gls-9DMavud2qs1ZQjWai5nuj52RmD8WpI8Q-Ps", 
+  apiUrl: "https://script.google.com/macros/s/AKfycbyXDQShKwAR0PzQ8QjgkXDqtWYNETXNeeWrIv7rp35N4reCT3NXULKYkAsoCRnb3JVM/exec"
+};
+
